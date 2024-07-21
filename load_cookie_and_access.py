a0af81adabf463f6df59942c1c3c1d2d6d8dae94
JAINAM576 <het170246@gmail.com>
2024-07-21 11:09:11 +0530

    new file added

--- load_cookie_and_access.py
+++ load_cookie_and_access.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pickle
+
 import time
 from selenium import webdriver 
 from selenium.webdriver.chrome.service import Service as ChromeService 
@@ -8,7 +8,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup 
 from selenium.webdriver.chrome.options import Options
 from time import sleep
 from flask import Flask
